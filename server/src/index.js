801a5a733b36e92868aa61e2134d64fb35eaa8e4
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2022-11-26 17:09:08 -0800

    Refactor

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const app = require('./src/app');
+const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
@@ -12,7 +12,11 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-  console.log(`User connected ${socket.id}`);
+  socket.on('update', (data) => {
+    socket.broadcast.emit('update', data);
+  });
 });
 
-server.listen(4000, () => console.log('Server is running on port 4000'));
+server.listen(4000, () =>
+  console.log('Server is listening on http://localhost:4000')
+);
