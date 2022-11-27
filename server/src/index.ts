281df336ae0b30cb99c03305145f02ce1c44a92f
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2022-11-27 11:44:02 -0800

    DB watch

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import http from 'http';
+import mongoose from 'mongoose';
 import app from './app';
 import { Server, Socket } from 'socket.io';
 
@@ -15,10 +16,35 @@ const io = new Server(server, {
   },
 });
 
-io.on('connection', (socket: Socket) => {});
+mongoose.connect(process.env.DATABASE_URL!);
+const db = mongoose.connection;
 
-server.listen(port, () => {
-  if (process.env.NODE_ENV !== 'production') {
-    console.log(`Server is listening on http://localhost:${port}`);
-  }
+db.once('open', () => {
+  console.log('Database connection established successfully');
+
+  const characterStream = db.collection('characters').watch();
+
+  characterStream.on('change', (change) => {
+    switch (change.operationType) {
+      case 'insert':
+        io.emit('insert', change.fullDocument);
+        break;
+      case 'update':
+        io.emit('update', change.fullDocument);
+        break;
+      case 'delete':
+        io.emit('delete', change.documentKey._id);
+        break;
+      default:
+        break;
+    }
+  });
 });
+
+io.on('connection', (socket: Socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  socket.on('disconnect', () => console.log(`User disconnected ${socket.id}`));
+});
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
